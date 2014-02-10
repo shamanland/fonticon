40b946f7db0f822d69eecac6ad1b71e0df6e9fde
Oleksii Kropachov <o.kropachov@shamanland.com>
2014-02-10 17:56:45 +0200

    Added icons to ActionBar.

--- app/src/main/java/com/shamanland/fonticon/CompoundIconsFragment.java
+++ app/src/main/java/com/shamanland/fonticon/CompoundIconsFragment.java
@@ -2,6 +2,8 @@ package com.shamanland.fonticon;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -10,5 +12,13 @@ public class CompoundIconsFragment extends ContentFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
         View result = inflater.inflate(R.layout.f_compound, container, false);
         return result;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.compound, menu);
+
+        menu.findItem(R.id.action_like).setIcon(FontIconDrawable.inflate(getResources(), R.xml.ic_like));
+        menu.findItem(R.id.action_group).setIcon(FontIconDrawable.inflate(getResources(), R.xml.ic_group));
     }
 }
--- app/src/main/java/com/shamanland/fonticon/GlowingIconsFragment.java
+++ app/src/main/java/com/shamanland/fonticon/GlowingIconsFragment.java
@@ -2,6 +2,8 @@ package com.shamanland.fonticon;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -10,5 +12,13 @@ public class GlowingIconsFragment extends ContentFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
         View result = inflater.inflate(R.layout.f_glowing, container, false);
         return result;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.glowing, menu);
+
+        menu.findItem(R.id.action_camera).setIcon(FontIconDrawable.inflate(getResources(), R.xml.ic_camera));
+        menu.findItem(R.id.action_settings).setIcon(FontIconDrawable.inflate(getResources(), R.xml.ic_settings));
     }
 }
