a5d9f6734e471cbd2b6cc9425b394503010943d8
Oleksii Kropachov <o.kropachov@shamanland.com>
2014-04-23 22:12:56 +0300

    Improved 'scaled icons' example - added SeekBar.

--- app/src/main/java/com/shamanland/fonticon/example/ScaledIconsFragment.java
+++ app/src/main/java/com/shamanland/fonticon/example/ScaledIconsFragment.java
@@ -8,18 +8,65 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.shamanland.fonticon.FontIconDrawable;
 
 public class ScaledIconsFragment extends ContentFragment {
+    private static final int MIN_SIZE = 10;
+    private static final int MAX_SIZE = 300;
+    private static final int DEFAULT_SIZE = 48;
+
     private ViewGroup mContainer;
+    private SeekBar mSeekBar;
+
+    private int mTextSize;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle state) {
         View result = inflater.inflate(R.layout.f_scaled, container, false);
+
         mContainer = (ViewGroup) result.findViewById(R.id.container);
+
+        mSeekBar = (SeekBar) result.findViewById(R.id.seek_bar);
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                setTextSize(progress);
+
+                if (fromUser) {
+                    updateContent();
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // empty
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // empty
+            }
+        });
+
+        if (state != null) {
+            setTextSize(state.getInt("text.size"));
+        } else {
+            setTextSize(DEFAULT_SIZE);
+        }
+
+        updateContent();
+
         return result;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle state) {
+        super.onSaveInstanceState(state);
+
+        state.putInt("text.size", mTextSize);
     }
 
     @Override
@@ -34,35 +81,32 @@ public class ScaledIconsFragment extends ContentFragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_scaleup:
-                scale(20);
+                setTextSize(mTextSize + 20);
+                updateContent();
                 return true;
 
             case R.id.action_scaledown:
-                scale(-20);
+                setTextSize(mTextSize - 20);
+                updateContent();
                 return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void scale(float value) {
-        int w = mContainer.getWidth();
+    protected void setTextSize(int value) {
+        mTextSize = Math.min(Math.max(MIN_SIZE, value), MAX_SIZE);
+    }
+
+    protected void updateContent() {
+        mSeekBar.setProgress(mTextSize);
 
         int count = mContainer.getChildCount();
         for (int i = 0; i < count; ++i) {
             View view = mContainer.getChildAt(i);
             if (view instanceof TextView) {
                 TextView tv = (TextView) view;
-
-                float newSize = tv.getTextSize() + value;
-
-                if (newSize > w) {
-                    newSize = w;
-                } else if (newSize < 10) {
-                    newSize = 10;
-                }
-
-                tv.setTextSize(TypedValue.COMPLEX_UNIT_PX, newSize);
+                tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, mTextSize);
             }
         }
     }
